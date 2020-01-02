24f2dcac9a9c7ff87cfe10c2afc53e6a09de0dbd
Karan Verma <hydroweaver@gmail.com>
2020-01-02 11:20:55 +0530

    wip

---
+++ reorderable_list/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Todo App with Re-orderable Lists & Swipe Cards",
+    home: App(),
+  ));
+}
+
+class App extends StatefulWidget{
+}
+
+class AppState extends State<App>{
+  
+}
+
