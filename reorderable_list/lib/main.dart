04f0dc8774c2be80d942518aad50dc4f059a1787
Author Karan Verma <hydroweaver@gmail.com>
2020-01-06 20:11:50 +0530

    added reorder

--- reorderable_list/lib/main.dart
+++ reorderable_list/lib/main.dart
@@ -7,10 +7,43 @@ void main(){
   ));
 }
 
+List<String> items = ["Item 1 - I will stick to the new position!", "Item 2 - I will stick to the new position!", "Item 3 - I will stick to the new position!"];
+
 class App extends StatefulWidget{
+  @override
+  AppState createState() {
+    return AppState();
+  }
 }
 
-class AppState extends State<App>{
-  
+class AppState extends State<App>{  
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Empty Half Baked App"),
+      ),
+      body: ReorderableListView(
+        children: <Widget>[
+          for(final value in items)
+            Text(
+              value,
+              key: Key(value),
+              style: TextStyle(
+                fontSize: 22.0
+              ),
+              )
+        ],
+        onReorder: (OldIndex, NewIndex){
+          setState(() {
+            if(OldIndex < NewIndex){
+              NewIndex -= 1;
+            }
+            var getReplacedWidget = items.removeAt(OldIndex);
+            items.insert(NewIndex, getReplacedWidget);
+          });
+        },
+      ),
+    );
+  }
 }
 
